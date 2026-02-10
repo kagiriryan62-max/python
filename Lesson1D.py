8ec65f3b0ee8d011561ddafeb782f2d1c6aaaf38
Kagiriryan-max <kagiriryan62@gmail.com>
2026-02-10 15:59:32 +0300

    Add file

---
+++ Lesson1D.py
@@ -0,0 +1,11 @@
+# Create a python program that is able to calculate the simple interest given the principle as 50000 rate as 5% and time as 8 years. SI =(p*I*t)/100
+principle = 50000
+rate = 5
+time = 8
+simpleinterest = (principle * rate * time)/100
+print("The simple interest is:", simpleinterest)
+
+# Assignment One Python
+# Write a python prigram that is able to calculate the BMI of a person whose weight is 78KGs and height is 1.75m. BMI =(weight/height squared).
+# Find the area and perimeter of a rectangle whose length is 48cm and width is 25cm.
+# Research on python list, tuple and dictionary types.
